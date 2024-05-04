94a86535d638b348592b2d4a8fc42e3459506e34
LUKMANFARIZ <lukmanfarizzz8@gmail.com>
2024-05-04 20:50:07 +0700

    menambahkan .editorconfig,.gitignore,application/,assets/,composer.json,index.php,license.txt,readme.rst,system/

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Lukman</li>
+        <li>Nama Belakang : Fariz</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Jalan Toyogiri Selatan Tambun Selatan Bekasi Timur</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>Sumedang</li>
+</ul>
+
+<h4>Olah Raga Favorit</h4>
+<ul type="square">
+    <li>Futsal</li>
+    <li>Fotografi</li>
+</ul>
+</section>
