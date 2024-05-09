755a72e38a26d374a2ff056eee9277c3343b2ff9
LUKMANFARIZ <lukmanfarizzz8@gmail.com>
2024-05-09 18:42:09 +0700

    menambahkan .htaccess,application/.htaccess,application/config/autoload.php,application/config/config.php,application/config/routes.php,application/controllers/Latihan1.php,application/controllers/Matakuliah.php,application/models/Model_latihan1.php,application/views/view-data-matakuliah.php,application/views/view-form-matakuliah.php,application/views/view-latihan1.php

---
+++ application/controllers/Matakuliah.php
@@ -0,0 +1,36 @@
+<?php
+class Matakuliah extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('view-form-matakuliah');
+    }
+
+    public function cetak()
+    {
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required|min_length[3]', [
+            'required' => 'Kode Matakuliah Harus diisi',
+            'min_length' => 'Kode terlalu pendek'
+        ]);
+
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_length[3]', [
+            'required' => 'Nama Matakuliah Harus diisi',
+            'min_length' => 'Nama terlalu pendek'
+        ]);
+
+        // Menjalankan validasi form
+        if ($this->form_validation->run() == false) {
+            // Jika validasi gagal, kembali ke halaman form dengan menampilkan pesan error
+            $this->load->view('view-form-matakuliah');
+        } else {
+            // Jika validasi berhasil, ambil data dari form dan tampilkan di halaman lain
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+
+            $this->load->view('view-data-matakuliah', $data);
+        }
+    }
+}
